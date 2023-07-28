8d5a6bf639db61f3ce9aca0ace7a79825029bdf5
irack000 <IRACK000@gmail.com>
2023-07-29 00:16:37 +0900

    Initial commit

---
+++ cdn/localSocket/localSocket.js
@@ -0,0 +1,28 @@
+function localSocket() {
+  localStorage.a = localStorage.b = JSON.stringify([]);
+  this.index = 0;
+  this.interval = setInterval(() => {
+    if (!this.in) {
+      if (!JSON.parse(localStorage.a).length) return;
+      this.in = "a"; this.out = "b";
+    }
+    var arr = JSON.parse(localStorage[this.in]);
+    if (arr.length <= this.index) return;
+    if (this.onmessage) this.onmessage({ data: JSON.parse(arr[this.index]) });
+    this.index++;
+  }, 200);
+  setTimeout(() => this.onopen && this.onopen({}));
+}
+localSocket.prototype = {
+  send: function(msg) {
+    if (!this.out) {
+      this.out = "a"; this.in = "b";
+    }
+    var arr = JSON.parse(localStorage[this.out]);
+    arr.push(JSON.stringify(msg));
+    localStorage[this.out] = JSON.stringify(arr);
+  },
+  close: function() {
+    clearInterval(this.interval);
+  }
+};
